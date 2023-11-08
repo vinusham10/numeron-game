925afbdf42fa652758aa941b2ae3eaca8c83ad29
vinusham10 <vinusham05@gmail.com>
2023-11-08 15:27:54 +0530

    game

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // Iteration 1: Make the Play button functional.
 // Description: When the Play button is clicked the game page should be displayed.
+let x = document.getElementById("play-button");
+  x.onclick = () => {
+    window.location.href = "game.html";
+  };
+  
